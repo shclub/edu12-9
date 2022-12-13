ae479b81b60a67fc593a8da5f2dd64a0c94d630d
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-12-13 14:11:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,108 +1,193 @@
-from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
-from bson.objectid import ObjectId
+#!/usr/bin/python
+import sqlite3
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-import yaml
 
 app = Flask(__name__)
-config = yaml.load(open('database.yaml'))
-client = MongoClient(config['uri'])
-# db = client.lin_flask
-db = client['knf-dev']
-CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config.from_pyfile('config.py')
 
-@app.route('/')
-def index():
-    return render_template('home.html')
+def connect_to_db():
+    conn = sqlite3.connect('database.db')
+    return conn
 
-@app.route('/users', methods=['POST', 'GET'])
-def data():
-    
-    # POST a data to database
-    if request.method == 'POST':
-        body = request.json
-        firstName = body['firstName']
-        lastName = body['lastName']
-        emailId = body['emailId'] 
-        # db.users.insert_one({
-        db['users'].insert_one({
-            "firstName": firstName,
-            "lastName": lastName,
-            "emailId":emailId
-        })
-        return jsonify({
-            'status': 'Data is posted to MongoDB!',
-            'firstName': firstName,
-            'lastName': lastName,
-            'emailId':emailId
-        })
-    
-    # GET all data from database
+
+def create_db_table():
+    try:
+        conn = connect_to_db()
+        conn.execute('''DROP TABLE employee''')
+        conn.execute('''
+            CREATE TABLE employee (
+                id INTEGER PRIMARY KEY NOT NULL,
+                empName TEXT NOT NULL,
+                empDeptName TEXT,
+                empTelNo TEXT,
+                empMail TEXT
+            );
+        ''')
+
+        conn.commit()
+        print("employee table created successfully")
+    except:
+        print("employee table creation failed - Maybe table")
+    finally:
+        conn.close()
+
+
+def insert_employee(employee):
+    inserted_employee = {}
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO employee (empName, empDeptName, empTelNo, empMail) VALUES (?, ?, ?, ?, ?)", (employee['empName'], employee['empDeptName'], employee['empTelNo'], employee['empMail']) )
+        conn.commit()
+        inserted_employee = get_employee_by_id(cur.lastrowid)
+    except:
+        conn().rollback()
+    finally:
+        conn.close()
+    return inserted_employee
+
+
+def get_employees():
+    employees = []
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM employee")
+        rows = cur.fetchall()
+
+        # convert row objects to dictionary
+        for i in rows:
+            employee = {}
+            employee["id"] = i["id"]
+            employee["empName"] = i["empName"]
+            employee["empDeptName"] = i["empDeptName"]
+            employee["empTelNo"] = i["empTelNo"]
+            employee["empMail"] = i["empMail"]
+            employees.append(employee)
+    except:
+        employees = []
+    return employees
+
+
+def get_employee_by_id(id):
+    employee = {}
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM employee WHERE id = ?", (id,))
+        row = cur.fetchone()
+
+        # convert row object to dictionary
+        employee["id"] = row["id"]
+        employee["empName"] = row["empName"]
+        employee["empDeptName"] = row["empDeptName"]
+        employee["empTelNo"] = row["empTelNo"]
+        employee["empMail"] = row["empMail"]
+    except:
+        employee = {}
+    return employee
+
+
+def update_employee(employee):
+    updated_employee = {}
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute("UPDATE employee SET empName = ?, empDeptName = ?, empTelNo = ?, empMail = ? WHERE id =?", (employee["empName"], employee["empDeptName"], employee["empTelNo"], employee["empMail"], employee["id"],))
+        conn.commit()
+        #return the employee
+        updated_employee = get_employee_by_id(employee["id"])
+    except:
+        conn.rollback()
+        updated_employee = {}
+    finally:
+        conn.close()
+    return updated_employee
+
+
+def delete_employee(id):
+    message = {}
+    try:
+        conn = connect_to_db()
+        conn.execute("DELETE from employee WHERE id = ?", (id))
+        conn.commit()
+        message["status"] = "employee deleted successfully"
+    except:
+        conn.rollback()
+        message["status"] = "Cannot delete employee"
+    finally:
+        conn.close()
+    return message
+
+
+employees = []
+employee0 = {
+    "name": "Charles Effiong",
+    "email": "charles@gamil.com",
+    "phone": "067765665656",
+    "address": "Lui Str, Innsbruck"
+}
+
+employee1 = {
+    "name": "Sam Adebanjo",
+    "email": "samadebanjo@gamil.com",
+    "phone": "098765465",
+    "address": "Sam Str, Vienna"
+}  
+
+employees.append(employee0)
+employees.append(employee1)
+
+create_db_table()
+
+#for i in employees:
+#    print(insert_employee(i))
+
+@app.route("/")
+def template_test():
+    return render_template(
+                'index.html',                      #렌더링 html 파일명
+                title="Flask Template Test",       #title 텍스트 바인딩1
+                my_str="Hello Flask!",             #my_str 텍스트 바인딩2
+                my_list=[x + 1 for x in range(10)] #30개 리스트 선언(1~30)
+            )
+  
+
+@app.route('/api/v1/employees', methods=['GET'])
+def api_get_employees():
+    return jsonify(get_employees())
+
+@app.route('/api/v1/employees/<id>', methods=['GET'])
+def api_get_employee(id):
+    return jsonify(get_employee_by_id(id))
+
+@app.route('/api/v1/employees',  methods = ['POST'])
+def api_add_employee():
+    employees = request.get_json()
+    return jsonify(insert_employee(employees))
+
+@app.route('/api/v1/employees/<id>',  methods = ['POST'])
+def api_update_employee():
+    employees = request.get_json()
+    return jsonify(update_employee(id,employees))
+
+@app.route('/api/v1/employee/<id>',  methods = ['POST'])
+def api_delete_employee(id):
+    return jsonify(delete_employee(id))
+
+# health_check
+@app.route('/health_check', methods = ['GET'])
+def health_check():
     if request.method == 'GET':
-        allData = db['users'].find()
-        dataJson = []
-        for data in allData:
-            id = data['_id']
-            firstName = data['firstName']
-            lastName = data['lastName']
-            emailId = data['emailId']
-            dataDict = {
-                'id': str(id),
-                'firstName': firstName,
-                'lastName': lastName,
-                'emailId': emailId
-            }
-            dataJson.append(dataDict)
-        print(dataJson)
-        return jsonify(dataJson)
-
-@app.route('/users/<string:id>', methods=['GET', 'DELETE', 'PUT'])
-def onedata(id):
-
-    # GET a specific data by id
-    if request.method == 'GET':
-        data = db['users'].find_one({'_id': ObjectId(id)})
-        id = data['_id']
-        firstName = data['firstName']
-        lastName = data['lastName']
-        emailId = data['emailId']
-        dataDict = {
-            'id': str(id),
-            'firstName': firstName,
-            'lastName': lastName,
-            'emailId':emailId
-        }
-        print(dataDict)
-        return jsonify(dataDict)
-        
-    # DELETE a data
-    if request.method == 'DELETE':
-        db['users'].delete_many({'_id': ObjectId(id)})
-        print('\n # Deletion successful # \n')
-        return jsonify({'status': 'Data id: ' + id + ' is deleted!'})
-
-    # UPDATE a data by id
-    if request.method == 'PUT':
-        body = request.json
-        firstName = body['firstName']
-        lastName = body['lastName']
-        emailId = body['emailId']
-
-        db['users'].update_one(
-            {'_id': ObjectId(id)},
-            {
-                "$set": {
-                    "firstName":firstName,
-                    "lastName":lastName,
-                    "emailId": emailId
-                }
-            }
-        )
-
-        print('\n # Update successful # \n')
-        return jsonify({'status': 'Data id: ' + id + ' is updated!'})
+        return json.dumps({'returnCode': 'OK'})
+    else:
+        return json.dumps({'returnCode': 'NG', 'message': 'Method ' + request.method + ' not allowed.'}, status=405)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0',port=8000,debug=True)
