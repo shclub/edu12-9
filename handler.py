3a4fe06b3ff0bb174ed3f90172068aa798345dc9
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-12-13 15:38:26 +0900

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,4 +1,6 @@
 import time
+import os
+import json
 
 def lambda_handler(event, context):   
     print("Lambda function ARN:", context.invoked_function_arn)
@@ -8,4 +10,14 @@ def lambda_handler(event, context):
     print("Lambda function memory limits in MB:", context.memory_limit_in_mb)
     # We have added a 1 second delay so you can see the time remaining in get_remaining_time_in_millis.
     time.sleep(1) 
+    json_region = os.environ['AWS_REGION']
     print("Lambda time remaining in MS:", context.get_remaining_time_in_millis())
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "Region ": json_region
+        })
+    }
