beb3920c85188eb1cd8f098eb4129e4a7a5f853d
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-12-13 17:08:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import boto3
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-patch_all()
+#logger = logging.getLogger()
+#logger.setLevel(logging.INFO)
+#patch_all()
 
 client = boto3.client('lambda')
 client.get_account_settings()
